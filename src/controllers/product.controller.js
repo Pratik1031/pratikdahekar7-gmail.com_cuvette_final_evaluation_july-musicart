1d43c7e5437c1a57d9cb68e90798bcde32070c0e
Pratik Dahekar <pratikdahekar7@gmail.com>
2024-03-25 01:41:07 +0530

    filter and sort added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -20,4 +20,49 @@ const product = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, productlist, "Products Fetched Successfully"));
 });
 
-module.exports = { product };
+const filteredProducts = asyncHandler(async (req, res) => {
+  const filters = {};
+  const sort = {};
+
+  if (req.body.brand) filters.brand = req.body.brand;
+  if (req.body.type) filters.type = req.body.type;
+  if (req.body.color) filters.color = req.body.color;
+  if (req.body.price) {
+    const priceRange = req.body.price.split("-").map(Number);
+    filters.price = { $gte: priceRange[0], $lte: priceRange[1] };
+
+    console.log(priceRange);
+    console.log(filters.price);
+    console.log(req.body.price);
+  }
+
+  if (req.body.sort_products) {
+    switch (req.body.sort_products) {
+      case "lowest":
+        sort.price = 1;
+        break;
+      case "highest":
+        sort.price = -1;
+        break;
+      case "atoz":
+        sort.brand = 1;
+        break;
+      case "ztoa":
+        sort.brand = -1;
+        break;
+      default:
+        sort.createdAt = -1;
+    }
+  }
+  const filtered = await Product.find(filters).sort(sort);
+
+  if (!filtered || filtered.length === 0) {
+    throw new ApiError(400, "Unable to find Data");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, filtered, "Data Filtered Successfully"));
+});
+
+module.exports = { product, filteredProducts };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const productController = require("../controllers/product.controller");
 
 router.route("/allProduct").get(productController.product);
+router.route("/filter").post(productController.filteredProducts);
 
 module.exports = router;
